73edfbde4ded553e3b76c5c03052136e51e6ce35
hiromi-in <72237080+hiromi-in@users.noreply.github.com>
2024-01-11 11:07:24 -0600

    Update Section5_Exercises.cs
    Add Q2

--- Section5_Exercises.cs
+++ Section5_Exercises.cs
@@ -18,3 +18,30 @@ public class Program
 		
 	}
 }
+//Q2
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		Console.WriteLine("Type two numbers: ");
+		Console.Write("First number: ");
+		var firstNum = Convert.ToInt32(Console.ReadLine());
+		Console.Write("Second number: ");
+		var secondNum = Convert.ToInt32(Console.ReadLine());
+		
+		if (firstNum > secondNum)
+		{
+		Console.WriteLine("The biggest number is {0}", firstNum);
+		}
+		else if (firstNum < secondNum)
+		{
+		Console.WriteLine("The biggest number is {0}", secondNum);
+		}
+		else
+		{
+		Console.WriteLine("You've typed the same number!");	
+		}
+	}
+}
