60c6e36506b00f8e44bb6ca0f38d073592a0fe0b
hiromi-in <72237080+hiromi-in@users.noreply.github.com>
2024-01-18 15:58:06 -0600

    Update Section5_Exercises2.cs

--- Section5_Exercises2.cs
+++ Section5_Exercises2.cs
@@ -18,3 +18,28 @@ public class Program
 	}
 }
 
+//Q2. Write a program and continuously ask the user to enter a number or "ok" to exit. Calculate the sum of all the previously entered numbers and display it on the console.
+
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		var keep_asking = true;
+		var count = 0;
+			
+		while (keep_asking){
+		    Console.WriteLine("Type a number or 'ok': ");
+			var answer = Console.ReadLine();
+			if (answer == "ok"){
+			    keep_asking = false;
+			}
+			else {
+			count += Convert.ToInt32(answer);
+			}	
+		}
+		
+		Console.WriteLine("The sum is {0} :)", count);
+	}
+}
